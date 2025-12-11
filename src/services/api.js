a1dc435a56dbbe8e984dfc8b02548411924edc38
bashfortech <141651763+123bashir@users.noreply.github.com>
2025-12-11 15:07:49 +0100

    Change API base URL to production server

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://videoplayer-backend-fieg.onrender.com/api';
 
 export const api = {
     // Get all lessons
